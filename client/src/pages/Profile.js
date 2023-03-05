b86b0a06d1b5c7057ad03c2137e180de8a3666b5
Marie P <map40j@gmail.com>
2023-03-04 17:04:19 -0800

    input the code for the profile page

---
+++ client/src/pages/Profile.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Profile = () => {
+  return (
+    <div>
+      <h1>Profile Page</h1>
+      <p>Welcome to your profile page!</p>
+      <p>Here you can view and edit your personal information.</p>
+      {/* Add your profile details and edit form here */}
+    </div>
+  );
+};
+
+export default Profile;
