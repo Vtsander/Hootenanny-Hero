ad876369622b54124c4cc29045e19804c8263d0b
Marie P <map40j@gmail.com>
2023-03-05 13:20:04 -0800

    working on the login code

--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -1,34 +1,61 @@
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import './login.css';
 
-const LoginForm = () => {
-  const [email, setEmail] = useState('');
+function Login({ onLogin }) {
+  const [emailOrUsername, setEmailOrUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [showLoginForm, setShowLoginForm] = useState(false);
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
+  const handleUsernameChange = (event) => {
+    setEmailOrUsername(event.target.value);
+  };
 
-    // Add your login logic here
-    console.log('Email:', email);
-    console.log('Password:', password);
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
 
-    // Clear form fields after submit
-    setEmail('');
-    setPassword('');
+  const handleLoginClick = () => {
+    setShowLoginForm(true);
+  };
+
+  const handleCancelClick = () => {
+    setShowLoginForm(false);
+  };
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    if (emailOrUsername === 'user' && password === 'password') {
+      onLogin(emailOrUsername);
+      setShowLoginForm(false);
+    } else {
+      // handle incorrect login details here
+    }
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        Email:
-        <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
-      </label>
-      <label>
-        Password:
-        <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-      </label>
-      <button type="submit">Log In</button>
-    </form>
+    <div className="form-container">
+      <button onClick={handleLoginClick}>Log In</button>
+      {showLoginForm && (
+        <div className="overlay">
+          <form onSubmit={handleFormSubmit} className="login-form">
+            <label>
+              Email or Username:
+              <input type="text" value={emailOrUsername} onChange={handleUsernameChange} />
+            </label>
+            <label>
+              Password:
+              <input type="password" value={password} onChange={handlePasswordChange} />
+            </label>
+            <div className="button-container">
+              <button type="button" onClick={handleCancelClick}>Cancel</button>
+              <button type="submit">Log In</button>
+            </div>
+          </form>
+        </div>
+      )}
+    </div>
   );
 };
 
-export default LoginForm;
+export default Login;
---
+++ src/components/pages/navbar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './navbar.css';
+
+function Navbar() {
+  return (
+    <nav>
+      <ul>
+        <li><a href="#home">Home</a></li>
+        <li><a href="#login">Login</a></li>
+        <li><a href="#events">Events</a></li>
+        <li><a href="#contactUS">Contact Us</a></li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
