88c364673be37dfff2a39477b8dc2a4a0316d141
Marie P <map40j@gmail.com>
2023-03-04 16:30:32 -0800

    fixed the syntaxt error in connect.js and restructured the files and the folders

--- server/schema/typeDefs.js
+++ server/schema/typeDefs.js
@@ -1,6 +1,29 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
-   
-`
-module.exports = typeDefs
+  type Event {
+    id: ID!
+    name: String!
+    date: String!
+    location: String!
+  }
+
+  type Query {
+    events: [Event!]!
+    event(id: ID!): Event!
+  }
+
+  input EventInput {
+    name: String!
+    date: String!
+    location: String!
+  }
+
+  type Mutation {
+    createEvent(input: EventInput!): Event!
+    updateEvent(id: ID!, input: EventInput!): Event!
+    deleteEvent(id: ID!): Boolean!
+  }
+`;
+
+module.exports = typeDefs;
