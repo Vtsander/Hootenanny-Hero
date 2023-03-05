08e0b0e2ab040a169b91e6fce0dbc47422289003
Marie P <map40j@gmail.com>
2023-03-04 16:49:03 -0800

    defined the db in server.js and fixed the syntax errors

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const path = require('path');
 const db = require('./config/connection');
 const { ApolloServer } = require('apollo-server-express');
 const { typeDefs, resolvers } = require('./schema')
+const db = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -10,7 +11,7 @@ const PORT = process.env.PORT || 3001;
 const server = new ApolloServer({
   typeDefs,
   resolvers
-})
+});
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
