2eedd1936b4df36ccf2c155985e72c68313026ee
jsherpss <sherpajingmey@yahoo.com>
2023-03-06 13:52:45 -0800

    added stripe back end

--- server/server.js
+++ server/server.js
@@ -3,7 +3,8 @@ const path = require('path');
 const db = require('./config/connection');
 const { ApolloServer } = require('apollo-server-express');
 const { typeDefs, resolvers } = require('./schema')
-
+var cors = require('cors');
+const stripe = require('stripe')(sk_test_51MgcUNKLzZz5BbzTZwGvKuxzAJJiC4n41ZFW67zVF0Utj7eZDbgqZn8jXhGq74HFskzIuu8s4dCQ1JjtRHe7zsKM00Uz64Z7pB)
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -11,9 +12,40 @@ const server = new ApolloServer({
   typeDefs,
   resolvers
 });
-
+const app = express();
+app.use(cors());
+app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.post("/checkout", async (req, res) => {
+  /*
+  req.body.items
+  [
+      {
+          id: 1,
+          quantity: 3
+      }
+  ]
+  stripe wants
+  [
+      {
+          price: 1,
+          quantity: 3
+      }
+  ]
+  */
+  console.log(req.body);
+    const items = req.body.items;
+    let lineItems = [];
+    items.forEach((item)=> {
+        lineItems.push(
+            {
+                price: item.id,
+                quantity: item.quantity
+            }
+        )
+    });
 
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
--- src/components/pages/navbar.js
+++ src/components/pages/navbar.js
@@ -1,16 +1,23 @@
 import React from 'react';
 import './navbar.css';
+import { useState } from 'react';
 
 function Navbar() {
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   return (
-    <nav>
-      <ul>
-        <li><a href="#home">Home</a></li>
-        <li><a href="#login">Login</a></li>
-        <li><a href="#events">Events</a></li>
-        <li><a href="/contactus">Contact Us</a></li>
-      </ul>
-    </nav>
+    <>
+      <nav>
+        <ul>
+          <li><a href="#home">Home</a></li>
+          <li><a href="#login">Login</a></li>
+          <li><a href="#events">Events</a></li>
+          <li><a href="/contactus">Contact Us</a></li>
+        </ul>
+      </nav>
+    </>
   );
 }
 
