70f789917bd72bf6594342b0a38bfc94d8e26d6c
Veroneca Sander <nguyenveroneca@yahoo.com>
2023-03-05 23:07:14 -0800

    adding profile code

---
+++ src/components/features/Profile.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+
+const ProfilePage = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [username, setUsername] = useState('')
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    createAccount();
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>  
+      <label>
+        Name:
+        <input type="name" value={name} onChange={(e) => setName(e.target.value)} />
+      </label>
+      <label>
+        Username:
+        <input type="username" value={username} onChange={(e) => setUsername(e.target.value)} />
+      </label>
+      <label>
+        Email:
+        <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+      </label>
+      <label>
+        Password:
+        <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+      </label>
+      <button type="submit">Save</button>
+    </form>
+  );
+};
+
+export default ProfilePage;
