a9255b8d5c167695a3c81d3025a48f034012dcf8
Marie P <map40j@gmail.com>
2023-03-06 02:18:12 -0800

    adjusted the styling for both the home page and login page, version 1

--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -1,11 +1,12 @@
 import React, { useState } from 'react';
-//import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import './login.css';
+import { useNavigate } from 'react-router-dom';
 
 function Login({ onLogin }) {
   const [emailOrUsername, setEmailOrUsername] = useState('');
   const [password, setPassword] = useState('');
-  const [showLoginForm, setShowLoginForm] = useState(false);
+  const navigate = useNavigate();
 
   const handleUsernameChange = (event) => {
     setEmailOrUsername(event.target.value);
@@ -15,47 +16,34 @@ function Login({ onLogin }) {
     setPassword(event.target.value);
   };
 
-  const handleLoginClick = () => {
-    setShowLoginForm(true);
-  };
-
-  const handleCancelClick = () => {
-    setShowLoginForm(false);
-  };
-
   const handleFormSubmit = (event) => {
     event.preventDefault();
-    if (emailOrUsername === 'user' && password === 'password') {
-      onLogin(emailOrUsername);
-      setShowLoginForm(false);
-    } else {
-      // handle incorrect login details here
-    }
+    navigate('/profile');
   };
 
   return (
-    <div className="form-container">
-      <button onClick={handleLoginClick}>Log In</button>
-      {showLoginForm && (
-        <div className="overlay">
-          <form onSubmit={handleFormSubmit} className="login-form">
-            <label>
-              Email or Username:
-              <input type="text" value={emailOrUsername} onChange={handleUsernameChange} />
-            </label>
-            <label>
-              Password:
-              <input type="password" value={password} onChange={handlePasswordChange} />
-            </label>
-            <div className="button-container">
-              <button type="button" onClick={handleCancelClick}>Cancel</button>
-              <button type="submit">Log In</button>
-            </div>
-          </form>
-        </div>
-      )}
-    </div>
+    <>
+      <h2>Login to start planning your next event!</h2>
+      <div className="form-container">
+        <form onSubmit={handleFormSubmit} className="login-form">
+          <label>
+            Email or Username:
+            <input type="text" value={emailOrUsername} onChange={handleUsernameChange} />
+          </label>
+          <label>
+            Password:
+            <input type="password" value={password} onChange={handlePasswordChange} />
+          </label>
+          <div className="button-container">
+            <button type="submit">Log In</button>
+            <button type="button">
+              <Link to="/">Cancel</Link>
+            </button>
+          </div>
+        </form>
+      </div>
+    </>
   );
-};
+}
 
 export default Login;
