cca3360e404ee11ec7f7c7cd9e308cf3c5413fda
Veroneca Sander <nguyenveroneca@yahoo.com>
2023-03-09 15:18:57 -0800

    fixing the budget warnings

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import ThankYou from './components/pages/ThankYou';
 import CreateAccount from './components/pages/CreateAccount';
 import Budget from './components/pages/Budget';
 import Congrats from './components/pages/congrats';
+import NewEvent from './components/pages/NewEvent';
 /*import LoggedInNavbar from './components/LoggedInNavbar';
 /*import firebase from 'firebase/app';
 import 'firebase/auth';
@@ -38,7 +39,7 @@ function App() {
             <Route path="/ThankYou" element={<ThankYou />} />
             <Route path="/Budget" element={<Budget />} />
            <Route path="/login/CreateAccount" element={<CreateAccount />} />
-           <Route path="/NewEvent" element={<CreateAccount />} />
+           <Route path="/NewEvent" element={<NewEvent />} />
            <Route path="/Congrats" element={<Congrats />} />
           </Routes>
         </div>
